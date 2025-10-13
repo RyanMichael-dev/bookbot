521d35bb353de794a19a22b6f27435b1d0295a02
ryanmichael <ryanmichael.dev@pm.me>
2025-10-12 23:07:00 -0400

    add get_num_words()

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from stats import get_num_words
+
 def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     print(book_text)
+    num_words = get_num_words(book_text)
+    print(f"Found {num_words} total words")
 
 
 def get_book_text(path):
