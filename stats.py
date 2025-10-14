ee3030ff87ada05ecb881fabb3a7e48326113b72
ryanmichael <ryanmichael.dev@pm.me>
2025-10-13 20:50:47 -0400

    add get_num_char

--- stats.py
+++ stats.py
@@ -4,3 +4,13 @@ def get_num_words(text):
     for word in words:
         count += 1
     return count
+
+def get_num_char(text):
+    char_dict = {}
+    words = text.lower()
+    for char in words:
+        if char not in char_dict:
+            char_dict[char] = 1
+        else:
+            char_dict[char] += 1
+    return char_dict
