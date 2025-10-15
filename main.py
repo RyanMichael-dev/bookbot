5c65b1534f729c11eb0095d7e35b3fa7ca4b5411
ryanmichael <ryanmichael.dev@pm.me>
2025-10-14 21:52:02 -0400

    updated main to include import sys, book_path = sys.argv[1]

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
+import sys
 from stats import get_num_words
 from stats import get_num_char
 from stats import get_sorted_dict
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
     book_text = get_book_text(book_path)
     num_words = get_num_words(book_text)
     char_count = get_num_char(book_text)
     sorted_char = get_sorted_dict(char_count)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count -----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count ---------")
